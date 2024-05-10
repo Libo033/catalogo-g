4d6c9bf4e7879819e86f924f5f50c8350016849d
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-05-10 16:52:20 -0300

    Creado API session

---
+++ src/app/api/v1/session/route.ts
@@ -0,0 +1,71 @@
+import { jwtVerify } from "jose";
+import jwt from "jsonwebtoken";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+export async function GET(req: Request) {
+  try {
+    const secret_key: string | undefined = process.env.JWT_SECRET;
+    const mySession: RequestCookie | undefined = cookies().get("mySession");
+    const user = process.env.USER;
+
+    if (!secret_key || !mySession || !user) {
+      return Response.json(
+        { Error: "credentials error", code: 500 },
+        { status: 500 }
+      );
+    }
+
+    if (mySession !== undefined) {
+      let secret_key: Uint8Array = new TextEncoder().encode("valentin");
+      const value = await jwtVerify(mySession.value, secret_key);
+
+      if (value.payload.user_id === user)
+        return Response.json({ admin: true, code: 200 }, { status: 200 });
+    }
+
+    return Response.json({ admin: false, code: 200 }, { status: 200 });
+  } catch (error) {
+    return Response.json({ Error: error, code: 500 }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const secret_key: string | undefined = process.env.JWT_SECRET;
+    const user = process.env.USER;
+    const pass = process.env.PASS;
+    const data: { email: string; password: string } = await req.json();
+
+    if (!secret_key || !user || !pass) {
+      return Response.json(
+        { Error: "credentials error", code: 500 },
+        { status: 500 }
+      );
+    }
+
+    if (data.email === user && data.password === pass) {
+      const my_token: string = jwt.sign(
+        {
+          user_id: user,
+        },
+        secret_key
+      );
+
+      cookies().set("mySession", my_token, {
+        httpOnly: true,
+        sameSite: "strict",
+        expires: Date.now() + 86400000 * 30, // 30 dias
+      });
+
+      return Response.json({ code: 200 }, { status: 200 });
+    }
+
+    return Response.json(
+      { Error: "forbidden session", code: 403 },
+      { status: 403 }
+    );
+  } catch (error) {
+    return Response.json({ Error: error, code: 500 }, { status: 500 });
+  }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { jwtVerify } from "jose";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const mySession: RequestCookie | undefined = request.cookies.get("mySession");
+
+  if (mySession !== undefined) {
+    let secret_key: Uint8Array = new TextEncoder().encode("valentin");
+    const value = await jwtVerify(mySession.value, secret_key);
+
+    if (value.payload.user_id) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/admin/dashboard", "/admin/dashboard/:path*"],
+};
