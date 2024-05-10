aa03f6a94b36c128bde94f6ea131c654248d06df
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-05-10 14:43:48 -0300

    Inicio Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,57 @@
-import Image from "next/image";
+"use client";
+import ProductCard from "@/components/card/ProductCard";
+import { IProducto } from "@/Libs/interfaces";
+import { Skeleton } from "@mui/material";
+import { useEffect, useState } from "react";
+
+interface ApiResponse {
+  code: number;
+  productos: IProducto[];
+}
 
 export default function Home() {
-  return <main></main>;
+  const [productos, setProductos] = useState<IProducto[]>();
+  const [search, setsearch] = useState<string>("");
+
+  useEffect(() => {
+    fetch("/api/v1/producto")
+      .then((res: Response) => res.json())
+      .then((data: ApiResponse) => {
+        if (data.code === 200) {
+          setProductos(data.productos);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <main>
+      <section className="p-4 mb-6 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {productos ? (
+          productos.length > 0 ? (
+            productos.map((prod) => <p></p>)
+          ) : (
+            <p className="h-80 flex justify-center items-center text-lg text-slate-600">
+              <ProductCard />
+            </p>
+          )
+        ) : (
+          <>
+            <Skeleton
+              width={"100%"}
+              sx={{ height: "456px", transform: "scale(1,1)" }}
+            />
+            <Skeleton
+              width={"100%"}
+              sx={{ height: "456px", transform: "scale(1,1)" }}
+            />
+            <Skeleton
+              width={"100%"}
+              sx={{ height: "456px", transform: "scale(1,1)" }}
+            />
+          </>
+        )}
+      </section>
+    </main>
+  );
 }
